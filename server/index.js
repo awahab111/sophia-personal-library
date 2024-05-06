bc9f337160a702c4863862500e6286404f7d3e76
Abdul Wahab <118908180+awahab111@users.noreply.github.com>
2024-05-06 16:32:20 +0500

    added enviroment variables

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 // server/server.js
 const express = require('express');
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mern';
-
+console.log(process.env.MONGODB_URI); 
 // Middleware
 app.use(express.json());
 
@@ -15,7 +14,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 
 // MongoDB Connection
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
