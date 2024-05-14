cb7dde7b67db5d672e3553e18851039541fce3d6
hassannawaz1210 <hassannawaz1210@gmail.com>
2024-05-14 23:54:03 +0500

    welcome page dynamic books

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 
-const BookCard = () => {
-  const book = {
-    title: "Book",
-    author: "Author",
-    description: "This is a dummy book ahahha This is a dummy book ahahha This is a dummy book ahahha",
-    coverImage: "https://letsenhance.io/static/66c1b6abf8f7cf44c19185254d7adb0c/28ebd/AiArtBefore.jpg",
-  };
+const BookCard = ({book}) => {
 
   return (
     //max width??? 
     <div className="max-w-sm bg-white rounded-[10px] overflow-hidden shadow-lg p-3 w-[160px] h-[260px] mx-auto">
       <img
         className="w-[130px] h-[171px] rounded-[5px] mx-auto"
-        src={book.coverImage}
+        src={book.cover}
         alt="Sunset in the mountains"
       />
       <div className="py-3 ml-1 mx-auto">
-        <h1 className="text-lg">{book.title}</h1>
+        <h1 className="text-md">{book.title}</h1>
         <p className="text-gray-500 text-sm">
             {book.author}
         </p>
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Navigation from "@/components/Navigation";
 import Searchbar from "@/components/Searchbar";
 import DateTime from "@/components/DateTime";
@@ -6,45 +6,70 @@ import BookCard from "@/components/BookCard";
 import Quotes from "@/pages/Welcome/Quotes";
 import Profile from "@/components/Profile";
 import Stats from "@/pages/Welcome/Stats";
-import Quote from 'inspirational-quotes';
-import MainPage from '../MainPage';
-import pdf from '/pdc.pdf';
-import 'pdfjs-viewer-element';
-import {useNavigate} from 'react-router-dom';
-import { useUserStore} from '@components/zuStore'
-import Loading from 'react-fullscreen-loading';
-
-
+import Quote from "inspirational-quotes";
+import MainPage from "../MainPage";
+import pdf from "/pdc.pdf";
+import "pdfjs-viewer-element";
+import { useNavigate } from "react-router-dom";
+import { useUserStore } from "@components/zuStore";
+import Loading from "react-fullscreen-loading";
+import axios from "axios";
 
 function Welcome() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const user = useUserStore(state => state.user);
+  const user = useUserStore((state) => state.user);
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
 
+  let [books, setBooks] = useState([]);
+ 
+  const links = [
+    "https://openlibrary.org/people/mekBot/books/want-to-read.json",
+    "https://openlibrary.org/people/mekBot/books/currently-reading.json",
+    "https://openlibrary.org/people/mekBot/books/already-read.json",
+  ];
+
   useEffect(() => {
     if (!user) {
-      navigate('/');
+      navigate("/");
     } else {
       setIsLoading(false);
     }
+
+
+    axios
+      .get(links[Math.floor(Math.random() * links.length)])
+      .then((response) => {
+        var temp = [];
+        for (let i = 0; i < 7; i++) {
+          temp.push({
+            title: response.data.reading_log_entries[i].work.title,
+            author: response.data.reading_log_entries[i].work.author_names[0],
+            cover: `https://covers.openlibrary.org/b/id/${response.data.reading_log_entries[i].work.cover_id}-L.jpg`,
+          });
+        }
+        setBooks(temp);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   if (isLoading) {
-    return <Loading loading background="#F3F3F7" loaderColor="#f89f76"/>;
+    return <Loading loading background="#F3F3F7" loaderColor="#f89f76" />;
   }
 
-  var quote_obj = Quote.getQuote({author: true});
+  var quote_obj = Quote.getQuote({ author: true });
   var random = Quote.getRandomQuote();
   if (quote_obj.text.length > 110) {
     quote_obj.text = random;
@@ -52,21 +77,19 @@ function Welcome() {
 
   return (
     <MainPage>
-      <div className='flex flex-wrap justify-center ' >
-        <Quotes quote={quote_obj.text} author={quote_obj.author}  />
-        <div className='w-[1%]' ></div> {/*this is a spacer*/}
-        <Stats/>
+      <div className="flex flex-wrap justify-center ">
+        <Quotes quote={quote_obj.text} author={quote_obj.author} />
+        <div className="w-[1%]"></div> {/*this is a spacer*/}
+        <Stats />
       </div>
-      <h1 className=" font-semibold text-[30px] text-gray-700 font-mono ml-1 " >Welcome Back Abdul Wahab</h1>
-      <h2 className='text-[20px] ml-2 ' > Currently Reading</h2>
+      <h1 className=" font-semibold text-[30px] text-gray-700 font-mono ml-1 ">
+        Welcome Back Abdul Wahab
+      </h1>
+      <h2 className="text-[20px] ml-2 "> Currently Reading</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:flex lg:flex-wrap sm:gap-x-5 gap-y-4 mx-1">
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
+        {books.map((book, index) => {
+          return <BookCard key={index} book={book} />;
+        })}
       </div>
     </MainPage>
   );
